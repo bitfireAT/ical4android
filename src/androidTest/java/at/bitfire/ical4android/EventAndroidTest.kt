ee0f87cd6fcea45b1c0ea3ab303df5ab21c01e18
Ricki Hirner <hirner@bitfire.at>
2020-06-11 00:50:18 +0200

    Fix tests

--- src/androidTest/java/at/bitfire/ical4android/EventAndroidTest.kt
+++ src/androidTest/java/at/bitfire/ical4android/EventAndroidTest.kt
@@ -1,26 +1,18 @@
 package at.bitfire.ical4android
 
-import net.fortuna.ical4j.model.TimeZone
 import net.fortuna.ical4j.model.component.VAlarm
-import net.fortuna.ical4j.model.component.VTimeZone
 import net.fortuna.ical4j.model.property.Attendee
 import net.fortuna.ical4j.model.property.DtEnd
 import net.fortuna.ical4j.model.property.DtStart
-import net.fortuna.ical4j.model.property.TzId
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import java.io.ByteArrayOutputStream
 import java.time.Duration
-import kotlin.concurrent.thread
 
 class EventAndroidTest {
 
     @Test
     fun testGenerateEtcUTC() {
-        val vtz = VTimeZone()
-        vtz.properties += TzId("Etc/UTC")
-        DateUtils.tzRegistry.register(TimeZone(vtz))
-
         val tzUTC = DateUtils.tzRegistry.getTimeZone("Etc/UTC")
 
         val e = Event()
@@ -31,8 +23,9 @@ class EventAndroidTest {
         e.attendees += Attendee("mailto:test@example.com")
         val baos = ByteArrayOutputStream()
         e.write(baos)
-        val ical = baos.toString(Charsets.UTF_8.toString())
-        assertEquals("a", ical)
+        val ical = baos.toString()
+
+        assertTrue("BEGIN:VTIMEZONE.+BEGIN:STANDARD.+END:STANDARD.+END:VTIMEZONE".toRegex(RegexOption.DOT_MATCHES_ALL).containsMatchIn(ical))
     }
 
 }
