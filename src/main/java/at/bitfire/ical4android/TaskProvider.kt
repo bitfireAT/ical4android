d92808f0a72d0787afc1bed35c76ff741f2ad335
Ricki Hirner <hirner@bitfire.at>
2022-11-24 12:41:43 +0100

    Change default preference order of task providers

--- src/main/java/at/bitfire/ical4android/TaskProvider.kt
+++ src/main/java/at/bitfire/ical4android/TaskProvider.kt
@@ -28,9 +28,9 @@ class TaskProvider private constructor(
             private val readPermission: String,
             private val writePermission: String
     ) {
-        OpenTasks("org.dmfs.tasks", "org.dmfs.tasks", 103, "1.1.8.2", PERMISSION_OPENTASKS_READ, PERMISSION_OPENTASKS_WRITE),
+        JtxBoard("at.techbee.jtx.provider", "at.techbee.jtx", 101010006, "1.01.01", PERMISSION_JTX_READ, PERMISSION_JTX_WRITE),
         TasksOrg("org.tasks.opentasks", "org.tasks", 100000, "10.0", PERMISSION_TASKS_ORG_READ, PERMISSION_TASKS_ORG_WRITE),
-        JtxBoard("at.techbee.jtx.provider", "at.techbee.jtx", 101010006, "1.01.01", PERMISSION_JTX_READ, PERMISSION_JTX_WRITE);
+        OpenTasks("org.dmfs.tasks", "org.dmfs.tasks", 103, "1.1.8.2", PERMISSION_OPENTASKS_READ, PERMISSION_OPENTASKS_WRITE);
 
         companion object {
             fun fromAuthority(authority: String): ProviderName {
