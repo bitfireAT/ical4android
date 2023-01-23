bd191964a4837a330b878f96d27d0dad8e447fe6
Arnau Mora <arnyminer.z@gmail.com>
2023-01-23 16:44:32 +0100

    Using `regexpForProblem` instead of `INVALID_DAY_PERIOD_REGEX`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/main/java/at/bitfire/ical4android/validation/FixInvalidDayOffsetPreprocessor.kt
+++ src/main/java/at/bitfire/ical4android/validation/FixInvalidDayOffsetPreprocessor.kt
@@ -8,12 +8,12 @@ package at.bitfire.ical4android.validation
  * Fixes durations with day offsets with the 'T' prefix.
  * See also https://github.com/bitfireAT/icsx5/issues/100
  */
-object FixInvalidDayOffsetPreprocessor: StreamPreprocessor() {
+object FixInvalidDayOffsetPreprocessor : StreamPreprocessor() {
 
-    private val INVALID_DAY_PERIOD_REGEX = Regex("^(DURATION|TRIGGER):-?PT-?\\d+D$",
-        setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE))
-
-    override fun regexpForProblem() = INVALID_DAY_PERIOD_REGEX
+    override fun regexpForProblem() = Regex(
+        "^(DURATION|TRIGGER):-?PT-?\\d+D$",
+        setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE),
+    )
 
     override fun fixString(original: String): String {
         var s: String = original
@@ -21,7 +21,7 @@ object FixInvalidDayOffsetPreprocessor: StreamPreprocessor() {
         // FIXME I have changed the regex (added ^, DURATION|TRIGGER and $)
 
         // Find all matches for the expression
-        val found = INVALID_DAY_PERIOD_REGEX.findAll(s)
+        val found = regexpForProblem().findAll(s)
         for (match in found) {
             // Get the range of the match
             val range = match.range
@@ -36,7 +36,7 @@ object FixInvalidDayOffsetPreprocessor: StreamPreprocessor() {
             // If the number has been converted to long correctly
             if (number != null) {
                 // Build a new string with the prefix given, and the number converted to hours
-                val newPiece = s.substring(start, numPos) + (number*24) + "H"
+                val newPiece = s.substring(start, numPos) + (number * 24) + "H"
                 // Replace the range found with the new piece
                 s = s.replaceRange(IntRange(start, end), newPiece)
             }
