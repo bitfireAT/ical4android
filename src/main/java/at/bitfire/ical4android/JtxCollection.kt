16a1500d57b0980d663154774f5c385436480180
Patrick Lang <72232737+patrickunterwegs@users.noreply.github.com>
2023-01-26 12:31:18 +0100

    Exclude recur instances from query dirty and query deleted code format updates removed deprecated method updateRelatedTo()

--- src/main/java/at/bitfire/ical4android/JtxCollection.kt
+++ src/main/java/at/bitfire/ical4android/JtxCollection.kt
@@ -102,7 +102,12 @@ open class JtxCollection<out T: JtxICalObject>(val account: Account,
      */
     fun queryDeletedICalObjects(): List<ContentValues> {
         val values = mutableListOf<ContentValues>()
-        client.query(JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account), null, "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ? AND ${JtxContract.JtxICalObject.DELETED} = ?", arrayOf(id.toString(), "1"), null).use { cursor ->
+        client.query(
+            JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account),
+            null,
+            "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ? AND ${JtxContract.JtxICalObject.DELETED} = ? AND ${JtxContract.JtxICalObject.RECURID} IS NULL", arrayOf(id.toString(), "1"),
+            null
+        ).use { cursor ->
             Ical4Android.log.fine("findDeleted: found ${cursor?.count} deleted records in ${account.name}")
             while (cursor?.moveToNext() == true) {
                 values.add(cursor.toValues())
@@ -117,7 +122,12 @@ open class JtxCollection<out T: JtxICalObject>(val account: Account,
      */
     fun queryDirtyICalObjects(): List<ContentValues> {
         val values = mutableListOf<ContentValues>()
-        client.query(JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account), null, "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ? AND ${JtxContract.JtxICalObject.DIRTY} = ?", arrayOf(id.toString(), "1"), null).use { cursor ->
+        client.query(
+            JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account),
+            null,
+            "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ? AND ${JtxContract.JtxICalObject.DIRTY} = ? AND ${JtxContract.JtxICalObject.RECURID} IS NULL", arrayOf(id.toString(), "1"),
+            null
+        ).use { cursor ->
             Ical4Android.log.fine("findDirty: found ${cursor?.count} dirty records in ${account.name}")
             while (cursor?.moveToNext() == true) {
                 values.add(cursor.toValues())
@@ -131,7 +141,12 @@ open class JtxCollection<out T: JtxICalObject>(val account: Account,
      * @return Content Values of the found item with the given filename or null if the result was empty or more than 1
      */
     fun queryByFilename(filename: String): ContentValues? {
-        client.query(JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account), null, "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ? AND ${JtxContract.JtxICalObject.FILENAME} = ?", arrayOf(id.toString(), filename), null).use { cursor ->
+        client.query(
+            JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account),
+            null,
+            "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ? AND ${JtxContract.JtxICalObject.FILENAME} = ?", arrayOf(id.toString(), filename),
+            null
+        ).use { cursor ->
             Ical4Android.log.fine("queryByFilename: found ${cursor?.count} records in ${account.name}")
             if (cursor?.count != 1)
                 return null
@@ -163,7 +178,12 @@ open class JtxCollection<out T: JtxICalObject>(val account: Account,
     fun updateSetFlags(flags: Int): Int {
         val values = ContentValues(1)
         values.put(JtxContract.JtxICalObject.FLAGS, flags)
-        return client.update(JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account), values, "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ? AND ${JtxContract.JtxICalObject.DIRTY} = ?", arrayOf(id.toString(), "0"))
+        return client.update(
+            JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account),
+            values,
+            "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ? AND ${JtxContract.JtxICalObject.DIRTY} = ?",
+            arrayOf(id.toString(), "0")
+        )
     }
 
     /**
@@ -187,100 +207,6 @@ open class JtxCollection<out T: JtxICalObject>(val account: Account,
         client.update(JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account), values, "${JtxContract.JtxICalObject.ICALOBJECT_COLLECTIONID} = ?", arrayOf(id.toString()))
     }
 
-
-    /**
-     * This function updates the Related-To relations in jtx Board.
-     * STEP 1: find entries to update (all entries with 0 in related-to). When inserting the relation, we only know the parent iCalObjectId and the related UID (but not the related iCalObjectId).
-     *         In this step we search for all Related-To relations where the LINKEDICALOBJEC_ID is not set, resolve it through the UID and set it.
-     * STEP 2/3: jtx Board saves the relations in both directions, the Parent has an entry for his Child, the Child has an entry for his Parent. Step 2 and Step 3 make sure, that the Child-Parent pair is
-     *         present in both directions.
-     */
-    @Deprecated("Moved to jtx Board content provider (function updateRelatedTo()). This function here will be deleted in one of the next versions.")
-    fun updateRelatedTo() {
-        // STEP 1: first find entries to update (all entries with 0 in related-to)
-        client.query(JtxContract.JtxRelatedto.CONTENT_URI.asSyncAdapter(account), arrayOf(JtxContract.JtxRelatedto.TEXT), "${JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID} = ?", arrayOf("0"), null).use {
-            while(it?.moveToNext() == true) {
-                val uid2upddate = it.getString(0)
-
-                client.query(JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account), arrayOf(JtxContract.JtxICalObject.ID), "${JtxContract.JtxICalObject.UID} = ?", arrayOf(uid2upddate), null).use { idOfthisUidCursor ->
-                    if (idOfthisUidCursor?.moveToFirst() == true) {
-                        val idOfthisUid = idOfthisUidCursor.getLong(0)
-
-                        val updateContentValues = ContentValues()
-                        updateContentValues.put(JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID, idOfthisUid)
-                        client.update(JtxContract.JtxRelatedto.CONTENT_URI.asSyncAdapter(account), updateContentValues,"${JtxContract.JtxRelatedto.TEXT} = ? AND ${JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID} = ?", arrayOf(uid2upddate, "0")
-                        )
-                    }
-                }
-            }
-        }
-
-
-        // STEP 2: query all related to that are linking their PARENTS and check if they also have the opposite relationship entered, if not, then add it
-        client.query(JtxContract.JtxRelatedto.CONTENT_URI.asSyncAdapter(account), arrayOf(JtxContract.JtxRelatedto.ICALOBJECT_ID, JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID, JtxContract.JtxRelatedto.RELTYPE), "${JtxContract.JtxRelatedto.RELTYPE} = ?", arrayOf(JtxContract.JtxRelatedto.Reltype.PARENT.name), null).use {
-                cursorAllLinkedParents ->
-            while (cursorAllLinkedParents?.moveToNext() == true) {
-                val childId = cursorAllLinkedParents.getString(0)
-                val parentId = cursorAllLinkedParents.getString(1)
-
-                client.query(JtxContract.JtxRelatedto.CONTENT_URI.asSyncAdapter(account), arrayOf(JtxContract.JtxRelatedto.ICALOBJECT_ID, JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID, JtxContract.JtxRelatedto.RELTYPE), "${JtxContract.JtxRelatedto.ICALOBJECT_ID} = ? AND ${JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID} = ? AND ${JtxContract.JtxRelatedto.RELTYPE} = ?", arrayOf(parentId.toString(), childId.toString(), JtxContract.JtxRelatedto.Reltype.CHILD.name), null).use {  cursor ->
-                    // if the query does not bring any result, then we insert the opposite relationship
-                    if (cursor?.moveToFirst() == false) {
-                        //get the UID of the linked entry
-                        client.query(JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account), arrayOf(JtxContract.JtxICalObject.UID), "${JtxContract.JtxICalObject.ID} = ?", arrayOf(childId.toString()), null).use {
-                                foundIcalObjectCursor ->
-
-                            if (foundIcalObjectCursor?.moveToFirst() == true) {
-                                val childUID = foundIcalObjectCursor.getString(0)
-                                val cv = ContentValues().apply {
-                                    put(JtxContract.JtxRelatedto.ICALOBJECT_ID, parentId)
-                                    put(JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID, childId)
-                                    put(JtxContract.JtxRelatedto.RELTYPE, JtxContract.JtxRelatedto.Reltype.CHILD.name)
-                                    put(JtxContract.JtxRelatedto.TEXT, childUID)
-                                }
-                                client.insert(JtxContract.JtxRelatedto.CONTENT_URI.asSyncAdapter(account), cv)
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-
-        // STEP 3: query all related to that are linking their CHILD and check if they also have the opposite relationship entered, if not, then add it
-        client.query(JtxContract.JtxRelatedto.CONTENT_URI.asSyncAdapter(account), arrayOf(JtxContract.JtxRelatedto.ICALOBJECT_ID, JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID, JtxContract.JtxRelatedto.RELTYPE), "${JtxContract.JtxRelatedto.RELTYPE} = ?", arrayOf(JtxContract.JtxRelatedto.Reltype.CHILD.name), null).use {
-                cursorAllLinkedParents ->
-            while (cursorAllLinkedParents?.moveToNext() == true) {
-
-                val parentId = cursorAllLinkedParents.getLong(0)
-                val childId = cursorAllLinkedParents.getLong(1)
-
-                client.query(JtxContract.JtxRelatedto.CONTENT_URI.asSyncAdapter(account), arrayOf(JtxContract.JtxRelatedto.ICALOBJECT_ID, JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID, JtxContract.JtxRelatedto.RELTYPE), "${JtxContract.JtxRelatedto.ICALOBJECT_ID} = ? AND ${JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID} = ? AND ${JtxContract.JtxRelatedto.RELTYPE} = ?", arrayOf(childId.toString(), parentId.toString(), JtxContract.JtxRelatedto.Reltype.PARENT.name), null).use {
-                        cursor ->
-
-                    // if the query does not bring any result, then we insert the opposite relationship
-                    if (cursor?.moveToFirst() == false) {
-
-                        //get the UID of the linked entry
-                        client.query(JtxContract.JtxICalObject.CONTENT_URI.asSyncAdapter(account), arrayOf(JtxContract.JtxICalObject.UID), "${JtxContract.JtxICalObject.ID} = ?", arrayOf(parentId.toString()), null).use {
-                                foundIcalObjectCursor ->
-
-                            if(foundIcalObjectCursor?.moveToFirst() == true) {
-                                val parentUID = foundIcalObjectCursor.getString(0)
-                                val cv = ContentValues().apply {
-                                    put(JtxContract.JtxRelatedto.ICALOBJECT_ID, childId)
-                                    put(JtxContract.JtxRelatedto.LINKEDICALOBJECT_ID, parentId)
-                                    put(JtxContract.JtxRelatedto.RELTYPE, JtxContract.JtxRelatedto.Reltype.PARENT.name)
-                                    put(JtxContract.JtxRelatedto.TEXT, parentUID)
-                                }
-                                client.insert(JtxContract.JtxRelatedto.CONTENT_URI.asSyncAdapter(account), cv)
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 
     /**
      * @return a string with all JtxICalObjects within the collection as iCalendar
