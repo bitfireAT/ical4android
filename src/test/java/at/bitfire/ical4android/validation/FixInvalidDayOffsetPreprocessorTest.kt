3adfb1216bc4bbd323a721cb64c9a1d151a22394
Arnau Mora <arnyminer.z@gmail.com>
2023-03-22 18:39:49 +0100

    Added multiple durations test
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- src/test/java/at/bitfire/ical4android/validation/FixInvalidDayOffsetPreprocessorTest.kt
+++ src/test/java/at/bitfire/ical4android/validation/FixInvalidDayOffsetPreprocessorTest.kt
@@ -4,15 +4,19 @@
 
 package at.bitfire.ical4android.validation
 
-import org.junit.Assert.*
-import org.junit.Test
 import java.time.Duration
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
 class FixInvalidDayOffsetPreprocessorTest {
 
     private fun fixStringAndAssert(expected: String, string: String) {
         val fixed = FixInvalidDayOffsetPreprocessor.fixString(string)
-        Duration.parse(fixed.substring(fixed.indexOf(':') + 1))
+        for (line in fixed.split('\n')) {
+            Duration.parse(line.substring(line.indexOf(':') + 1))
+        }
         assertEquals(expected, fixed)
     }
 
@@ -37,6 +41,24 @@ class FixInvalidDayOffsetPreprocessorTest {
     fun test_FixString_DayOffsetFrom_Valid() {
         fixStringAndAssert("DURATION:-PT12H", "DURATION:-PT12H")
         fixStringAndAssert("TRIGGER:-PT12H", "TRIGGER:-PT12H")
+    }
+
+    @Test
+    fun test_FixString_DayOffsetFromMultiple_Invalid() {
+        fixStringAndAssert("DURATION:-P1D\nTRIGGER:-P2D", "DURATION:-PT1D\nTRIGGER:-PT2D")
+
+        fixStringAndAssert("DURATION:-P1D\nTRIGGER:-P2D", "DURATION:-P1DT\nTRIGGER:-P2DT")
+    }
+
+    @Test
+    fun test_FixString_DayOffsetFromMultiple_Valid() {
+        fixStringAndAssert("DURATION:-PT12H\nTRIGGER:-PT12H", "DURATION:-PT12H\nTRIGGER:-PT12H")
+    }
+
+    @Test
+    fun test_FixString_DayOffsetFromMultiple_Mixed() {
+        fixStringAndAssert("DURATION:-P1D\nDURATION:-PT12H\nTRIGGER:-P2D", "DURATION:-PT1D\nDURATION:-PT12H\nTRIGGER:-PT2D")
+        fixStringAndAssert("DURATION:-P1D\nDURATION:-PT12H\nTRIGGER:-P2D", "DURATION:-P1DT\nDURATION:-PT12H\nTRIGGER:-P2DT")
     }
 
     @Test
