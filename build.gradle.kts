51a459601ace8ca04414118f7e1f2cb95c74016a
Arnau Mora <arnyminerz@proton.me>
2023-08-28 12:10:10 +0200

    Updated AGP to 8.1.0 and Kotlin to 1.9.0
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@
  **************************************************************************************************/
 
 plugins {
-    id("com.android.library") version "8.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
-    id("org.jetbrains.dokka") version "1.8.10" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.dokka") version "1.9.0" apply false
 }
 
 group = "at.bitfire"
