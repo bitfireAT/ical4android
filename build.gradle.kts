eb6e5f65d6ec3285e5188a46f8a305a78238b0a9
Arnau Mora <arnyminerz@proton.me>
2023-08-28 12:11:34 +0200

    Set Dokka to latest version
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@
 plugins {
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("org.jetbrains.dokka") version "1.9.0" apply false
+    id("org.jetbrains.dokka") version "1.8.20" apply false
 }
 
 group = "at.bitfire"
