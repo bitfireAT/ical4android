189e24ac36cfe28da5fe0b3f298ff5c1fdfde31c
Arnau Mora <arnyminerz@proton.me>
2024-01-02 12:21:59 +0100

    Upgrade Kotlin
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@
 
 plugins {
     id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.dokka") version "1.8.20" apply false
 }
 
