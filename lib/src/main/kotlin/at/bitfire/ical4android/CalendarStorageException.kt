a3e886c738a9b87f0ea8e7a22e900316eacaf3ec
Ricki Hirner <hirner@bitfire.at>
2024-05-07 20:36:02 +0200

    Don't use CalendarStorageException to wrap RemoteException / as all-catch (#151)
    * Don't use CalendarStorageException to wrap RemoteException / as all-catch
    
    * BatchOperation: wrap RuntimeException

--- lib/src/main/kotlin/at/bitfire/ical4android/CalendarStorageException.kt
+++ lib/src/main/kotlin/at/bitfire/ical4android/CalendarStorageException.kt
@@ -4,6 +4,11 @@
 
 package at.bitfire.ical4android
 
+/**
+ * Indicates a problem with a calendar storage operation, like when a row can't be inserted or updated.
+ *
+ * Should not be used to wrap [android.os.RemoteException].
+ */
 class CalendarStorageException: Exception {
 
     constructor(message: String): super(message)
