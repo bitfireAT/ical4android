aa1d0ec798750c1d09d4e03b273b6aba160c2b50
Arnau Mora Gras <arnyminerz@proton.me>
2024-08-14 11:22:27 +0200

    Assume SDK level
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- lib/src/androidTest/kotlin/at/bitfire/ical4android/ICalPreprocessorTest.kt
+++ lib/src/androidTest/kotlin/at/bitfire/ical4android/ICalPreprocessorTest.kt
@@ -4,10 +4,12 @@
 
 package at.bitfire.ical4android
 
+import android.os.Build
 import at.bitfire.ical4android.validation.FixInvalidDayOffsetPreprocessor
 import at.bitfire.ical4android.validation.FixInvalidUtcOffsetPreprocessor
 import at.bitfire.ical4android.validation.ICalPreprocessor
 import io.mockk.mockkObject
+import io.mockk.spyk
 import io.mockk.verify
 import net.fortuna.ical4j.data.CalendarBuilder
 import net.fortuna.ical4j.model.Component
@@ -16,11 +18,17 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.InputStreamReader
 import java.io.StringReader
+import org.junit.Assume.assumeTrue
 
 class ICalPreprocessorTest {
 
     @Test
     fun testPreprocessStream_appliesStreamProcessors() {
+        assumeTrue(
+            "Can only run on API Level 24 or newer because mockkObject doesn't support Android < P (SDK 28)",
+            Build.VERSION.SDK_INT >= 28
+        )
+
         mockkObject(FixInvalidDayOffsetPreprocessor, FixInvalidUtcOffsetPreprocessor) {
             ICalPreprocessor.preprocessStream(StringReader(""))
 
