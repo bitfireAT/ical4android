6a5df96af9f0d4e12da887e96b707ac49e7f5bf4
Arnau Mora <arnyminerz@proton.me>
2024-08-17 12:30:47 +0200

    Forced dependency versions and excluded dependencies (#165)
    * Forced dependency versions and excluded dependencies
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Forced dependency versions and excluded dependencies
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Changed notation
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Assume SDK level
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Using `PackageInfoCompat`
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    * Using `SdkSuppress`
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>
    
    ---------
    
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- lib/src/androidTest/kotlin/at/bitfire/ical4android/ICalPreprocessorTest.kt
+++ lib/src/androidTest/kotlin/at/bitfire/ical4android/ICalPreprocessorTest.kt
@@ -4,23 +4,26 @@
 
 package at.bitfire.ical4android
 
+import androidx.test.filters.SdkSuppress
 import at.bitfire.ical4android.validation.FixInvalidDayOffsetPreprocessor
 import at.bitfire.ical4android.validation.FixInvalidUtcOffsetPreprocessor
 import at.bitfire.ical4android.validation.ICalPreprocessor
 import io.mockk.mockkObject
 import io.mockk.verify
+import java.io.InputStreamReader
+import java.io.StringReader
 import net.fortuna.ical4j.data.CalendarBuilder
 import net.fortuna.ical4j.model.Component
 import net.fortuna.ical4j.model.component.VEvent
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.io.InputStreamReader
-import java.io.StringReader
 
 class ICalPreprocessorTest {
 
     @Test
+    @SdkSuppress(minSdkVersion = 28)
     fun testPreprocessStream_appliesStreamProcessors() {
+        // Can only run on API Level 28 or newer because mockkObject doesn't support Android < P
         mockkObject(FixInvalidDayOffsetPreprocessor, FixInvalidUtcOffsetPreprocessor) {
             ICalPreprocessor.preprocessStream(StringReader(""))
 
