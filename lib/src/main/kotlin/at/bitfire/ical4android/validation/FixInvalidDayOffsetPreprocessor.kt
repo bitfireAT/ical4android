c631e22003d1f12f1e487c17eb29e44caab88da7
Sunik Kupfer <kupfer@bitfire.at>
2024-11-13 12:57:14 +0100

    Use capturing group; Add comments

--- lib/src/main/kotlin/at/bitfire/ical4android/validation/FixInvalidDayOffsetPreprocessor.kt
+++ lib/src/main/kotlin/at/bitfire/ical4android/validation/FixInvalidDayOffsetPreprocessor.kt
@@ -14,26 +14,34 @@ object FixInvalidDayOffsetPreprocessor : StreamPreprocessor() {
         // Examples:
         // TRIGGER:-P2DT
         // TRIGGER:-PT2D
-        // REFRESH-INTERVAL;VALUE=DURATION:PT1D
-        "(?:^|(^(DURATION|REFRESH-INTERVAL|RELATED-TO|TRIGGER));VALUE=)(DURATION|TRIGGER):-?P((T-?\\d+D)|(-?\\d+DT))\$",
+        // REFRESH-INTERVAL;VALUE=DURATION:-PT1D
+        "(?:^|^(?:DURATION|REFRESH-INTERVAL|RELATED-TO|TRIGGER);VALUE=)(?:DURATION|TRIGGER):(-?P((T-?\\d+D)|(-?\\d+DT)))$",
         setOf(RegexOption.MULTILINE, RegexOption.IGNORE_CASE)
     )
 
     override fun fixString(original: String): String {
-        var s: String = original
+        var iCal: String = original
 
         // Find all instances matching the defined expression
-        val found = regexpForProblem().findAll(s)
+        val found = regexpForProblem().findAll(iCal)
 
         // ..and repair them
         for (match in found) {
-            val matchStr = match.value
-            val fixed = matchStr
+            // Fix the duration string
+            val faultyDuration = match.groupValues[1]       // IE: "-PT1D" (faulty)
+            val fixedDuration = faultyDuration              // IE: "-P1D" (fixed)
                 .replace("PT", "P")
                 .replace("DT", "D")
-            s = s.replace(matchStr, fixed)
+
+            // Replace the faulty duration with the fixed one in the captured line
+            val faultyCapture = match.value                 // IE: "REFRESH-INTERVAL;VALUE=DURATION:-PT1D" (faulty)
+            val fixedCapture = faultyCapture                // IE: "REFRESH-INTERVAL;VALUE=DURATION:-P1D" (fixed)
+                .replace(faultyDuration, fixedDuration)
+
+            // Replace complete faulty line in the iCal string with the fixed one
+            iCal = iCal.replace(faultyCapture, fixedCapture)
         }
-        return s
+        return iCal
     }
 
 }
